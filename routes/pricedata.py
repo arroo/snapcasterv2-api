8e0593aa1b865e4f545471131e9d2fde0bd3f630
Bryce Eppler <eppler97@gmail.com>
2023-01-23 08:12:21 -0800

    pricedata endpoint update to run background tasks for scraping

--- routes/pricedata.py
+++ routes/pricedata.py
@@ -5,15 +5,70 @@ from datetime import datetime
 import psycopg2
 import os
 from fastapi import BackgroundTasks, APIRouter
-
+import json
+import redis
+import concurrent.futures
+# import the search_single function from search.py
+from routes.search import increment_frequency, fetchScrapers, post_price_entry
 
 router = APIRouter()
+rd = redis.Redis(host=os.environ['RD_HOST'], port=os.environ['RD_PORT'], password=os.environ['RD_PASSWORD'], db=0)# get request where query params are the card name
 
-# get request where query params are the card name
+# Background tasks
+# Scrape a single card by calling @router.post("/single/") from search.py
+def scrape_single_card(cardName: str, background_tasks: BackgroundTasks):
+    """
+    Search for a single card and return all prices across the provided websites
+    """
+    websites = ["all"]
+    # Scraper function
+    def transform(scraper):
+        scraper.scrape()
+        scraperResults = scraper.getResults()
+        for result in scraperResults:
+            results.append(result)
+        return
+
+    # List to store results from all threads
+    results = []
+    # cardName lowercased
+    cache = rd.get(cardName.lower())
+    if cache:
+        background_tasks.add_task(increment_frequency, cardName.lower())
+        return json.loads(cache)
+    else :
+        scraperMap = fetchScrapers(cardName)
+        # Filter out scrapers that are not requested in request.websites
+        try:
+            if "all" in websites:
+                scrapers = scraperMap.values()
+            else:
+                scrapers = [scraperMap[website] for website in websites]
+        except KeyError:
+            return {"error": "Invalid website provided"}
+
+        # Run scrapers in parallel
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            threadResults = executor.map(transform, scrapers)
+
+        # Create a new search object and post it to the database
+        numResults = len(results)
+        log = Search(query=cardName, websites=','.join(websites), query_type="single",
+                    results="", num_results=numResults, timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        SQLModel.metadata.create_all(engine)
+        session = Session(engine)
+        session.add(log)
+        session.commit()
+        session.close()
+        background_tasks.add_task(post_price_entry, results)
+        rd.set(cardName.lower(), json.dumps(results))
+        rd.expire(cardName.lower(), 120)
+        return results
+
 
 
 @router.get("/card/{card_name}")
-async def get_card(card_name: str):
+async def get_card(card_name: str, background_tasks: BackgroundTasks):
     conn = psycopg2.connect(
         dbname=os.environ['PG_DB'],
         user=os.environ['PG_USER'],
@@ -44,6 +99,22 @@ async def get_card(card_name: str):
     )
     rows = cur.fetchall()
 
+    
+    # if there is no price_entry with date = today, then scrape the card
+    todaysDate = datetime.now().strftime("%Y-%m-%d")
+    try:
+        print("rows: " + str(rows))
+        mostRecentPricelistDate = rows[len(rows) - 1][0].strftime("%Y-%m-%d")
+    except IndexError:
+        mostRecentPricelistDate = ""
+    if len(rows) == 0 or mostRecentPricelistDate != todaysDate:
+        print("scraping card: " + card_name)
+        background_tasks.add_task(scrape_single_card, card_name, background_tasks)
+        return {
+            "card_name": card_name,
+            "image_uri": "",
+            "price_data": []
+        }
     try:
         cardName = rows[0][4]
         imageUri = rows[0][5]
