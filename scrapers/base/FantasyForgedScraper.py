4e11a561c4f0a8f952f4dfefa3aab4d96337c4bc
Bryce Eppler <eppler97@gmail.com>
2023-05-24 07:20:34 -0700

    Fantasy forged bug fixes for single search

--- scrapers/base/FantasyForgedScraper.py
+++ scrapers/base/FantasyForgedScraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from .Scraper import Scraper
+import sys
 
 
 class FantasyForgedScraper(Scraper):
@@ -25,18 +26,25 @@ class FantasyForgedScraper(Scraper):
         # " = %22
         urlCardName = self.cardName.replace(' ', '%20').replace('/', '%2F').replace("'", "%27").replace(',', '%2C')
         return self.searchUrl + urlCardName + '&options%5Bprefix%5D=last&filter_availability=in-stock'
-        
+       
+       
 
     def scrape(self):
         page = requests.get(self.url)
+        
         sp = BeautifulSoup(page.text, 'html.parser')
-        cards = sp.select('li.grid__item')
-
+        cards = sp.select('div.grid__item')
+        
         for card in cards:
             try:
                 # We check for in stock in the search link, don't need to check here
-                
-                cardName = card.select_one('a.full-unstyled-link').getText().strip()
+                try: 
+                    cardName = card.select_one('div.grid-view-item__title').getText().strip()
+                except:
+                    # print(f'No card name in the following html')
+                    # print(card)
+                    # print()
+                    continue
                 if "Art Card".lower() in cardName.lower():
                     continue
                 foil = False
@@ -48,16 +56,28 @@ class FantasyForgedScraper(Scraper):
                 cardSet = ""
 
 
+                try:
+                    link = self.baseUrl + card.select_one('div.grid-view-item__link div.product-card-list2__image-wrapper > a')['href']
+                except:
+                    # print(f'No link in the following html')
+                    # print(card)
+                    # print()
+                    continue
 
-                link = self.baseUrl + card.select_one('a.full-unstyled-link')['href']
-                image = "https:" + card.select_one('div.media img')['srcset'].split(' ')[0].replace('1x', '2x')
+                try:
+                    image = "https:" + card.select_one('div.image-inner img')['src'].split(' ')[0].replace('1x', '2x')
+                except:
+                    print(f'No image in the following html')
+                    print(card)
+                    print()
+                    image = ""
+                    continue
 
                 # Verify card name is correct
                 if not self.compareCardNames(self.cardName.lower(), cardName.lower()):
                     continue
 
-                price = card.select_one('span.price-item--regular').getText().replace('$', '').replace("CAD", "").replace(',', '').strip()
-
+                price = card.select_one('div.product-card-list2__details.product-description > div.grid-view-item__meta > div > span.product-price__price.is-bold.qv-regularprice').getText().replace('$', '').replace("CAD", "").replace(',', '').strip()
                 # Since FantasyForged has multiple stores, they could have multiple cards with same condition and set
                 # We need to check if an identical card already exists in the list
                 cardToAdd = {
@@ -75,7 +95,7 @@ class FantasyForgedScraper(Scraper):
                     self.results.append(cardToAdd)
 
             except Exception as e:
-                print(f'Error searching for {self.cardName} on {self.website}')
+                print(f'Error searching for {self.cardName} on {self.website} on line {sys.exc_info()[-1].tb_lineno}')
                 print(e.args[-5:])
                 continue
         
