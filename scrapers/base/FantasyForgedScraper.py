e2091c3850fb413ee2f876f010aa9778919a98e7
Bryce Eppler <eppler97@gmail.com>
2023-07-11 09:06:33 -0700

    adding fantasy forged for single scraper again

--- scrapers/base/FantasyForgedScraper.py
+++ scrapers/base/FantasyForgedScraper.py
@@ -4,14 +4,16 @@ from .Scraper import Scraper
 import sys
 
 
+
 class FantasyForgedScraper(Scraper):
     """
-    Split cards can be searched using "//" as a split
+    TODO:
+    - properly parse all available conditions and foils, right now we just hardcode NM and non-foil
     """
     def __init__(self, cardName):
         Scraper.__init__(self, cardName)
         self.baseUrl = 'https://FantasyForged.ca'
-        self.searchUrl = self.baseUrl + '/a/search?q='
+        self.searchUrl = self.baseUrl + '/search?options%5Bprefix%5D=last&type=product&q='
         self.url = self.createUrl()
         self.website = 'fantasyforged'
 
@@ -24,17 +26,30 @@ class FantasyForgedScraper(Scraper):
         # ' = %27
         # , = %2C
         # " = %22
-        urlCardName = self.cardName.replace(' ', '%20').replace('/', '%2F').replace("'", "%27").replace(',', '%2C')
-        return self.searchUrl + urlCardName + '&options%5Bprefix%5D=last&filter_availability=in-stock'
+        urlCardName = self.cardName.replace(' ', '+').replace('/', '%2F').replace("'", "%27").replace(',', '%2C')
+        return self.searchUrl + urlCardName# + '&options%5Bprefix%5D=last&filter_availability=in-stock'
        
        
 
     def scrape(self):
         page = requests.get(self.url)
+
+        # log infor about the request
+        # Log information about response, status code, and url, number of results
+        print(f"-----------------------------------")
+        print(f"Response: {page.status_code}")
+        print(f"Response: {page.reason}")
+        print(f"URL: {page.url}")
+        # print(f"Number of results: {
+        print(f"-----------------------------------")
+        
         
         sp = BeautifulSoup(page.text, 'html.parser')
-        cards = sp.select('div.grid__item')
+        cards = sp.select('div.products-display div.product-card-list2')
         
+        print(f"Number of results: {len(cards)}")
+        print(f"-----------------------------------")
+
         for card in cards:
             try:
                 
@@ -50,29 +65,26 @@ class FantasyForgedScraper(Scraper):
                     continue
                 if "Art Card".lower() in cardName.lower():
                     continue
-                foil = False
-                if "(Foil)" in cardName:
-                    foil = True
+
 
                 # remove any brackets and their contents from the card name
                 cardName = cardName.split(' (')[0].strip()
-                cardSet = ""
+                # card set is inside square brackets in card name
+                if '[' in cardName:
+                    cardSet = cardName.split('[')[1].split(']')[0].strip()
+                    cardName = cardName.split('[')[0].strip()
 
+                else :
+                    cardSet = ""
 
                 try:
                     link = self.baseUrl + card.select_one('div.grid-view-item__link div.product-card-list2__image-wrapper > a')['href']
                 except:
-                    # print(f'No link in the following html')
-                    # print(card)
-                    # print()
                     continue
 
                 try:
                     image = "https:" + card.select_one('div.image-inner img')['src'].split(' ')[0].replace('1x', '2x')
                 except:
-                    print(f'No image in the following html')
-                    print(card)
-                    print()
                     image = ""
                     continue
 
@@ -81,14 +93,12 @@ class FantasyForgedScraper(Scraper):
                     continue
 
                 price = card.select_one('div.product-card-list2__details.product-description > div.grid-view-item__meta > div > span.product-price__price.is-bold.qv-regularprice').getText().replace('$', '').replace("CAD", "").replace(',', '').strip()
-                # Since FantasyForged has multiple stores, they could have multiple cards with same condition and set
-                # We need to check if an identical card already exists in the list
                 cardToAdd = {
                     'name': cardName,
                     'image': image,
                     'link': link,
                     'set': cardSet,
-                    'foil': foil,
+                    'foil': False,
                     'condition': "NM",
                     'price': float(price),
                     'website': self.website
